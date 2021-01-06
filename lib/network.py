f4beca4ac93f209537468373fb6b26ebb7e38317
EddWills95 <edd.williams@me.com>
2021-01-06 18:33:55 +0000

    remove comments

--- lib/network.py
+++ lib/network.py
@@ -1,11 +1,8 @@
 import socket 
-  
-# Function to display hostname and 
-# IP address 
+
 def get_ip(): 
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     try:
-        # doesn't even have to be reachable
         s.connect(('10.255.255.255', 1))
         IP = s.getsockname()[0]
     except Exception:
