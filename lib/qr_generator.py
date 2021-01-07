a8ee19f818d4c8cf3a27c60df45df6ea81aa08a6
EddWills95 <edd.williams@me.com>
2021-01-07 09:56:09 +0000

    increase size and scale of info sections

--- lib/qr_generator.py
+++ lib/qr_generator.py
@@ -4,21 +4,23 @@ import os
 
 QR_file_name = "QR.png"
 QR_mini_img = "assets/umbrel-qr-icon.png"
+size = (160, 160)
 
 def generate_qr_code(data):
-    container = pygame.Surface((100, 100))
+    container = pygame.Surface(size)
     container.fill((255,255,255, 255))
     
     qr = pyqrcode.create(data)
-    qr.png(QR_file_name, scale=4, module_color=(0,0,0), background=(255,255,255), quiet_zone=0)
+    qr.png(QR_file_name, scale=3, module_color=(0,0,0), background=(255,255,255), quiet_zone=0)
     img = pygame.image.load(QR_file_name)
+    img = pygame.transform.scale(img, size)
     
     # Get the rect of the qr code image
     img_rect = img.get_rect()
 
     # Load and scale mini image
     mini_img = pygame.image.load(QR_mini_img).convert_alpha()
-    mini_img = pygame.transform.smoothscale(mini_img, (25, 25))
+    mini_img = pygame.transform.scale(mini_img, (35, 35))
     
     mini_img_rect = mini_img.get_rect(center=(img_rect.width / 2, img_rect.height / 2))
 
